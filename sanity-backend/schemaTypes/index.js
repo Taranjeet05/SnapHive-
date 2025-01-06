e1d677a551aa601269a9ccb9de44aca93952c5af
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-06 17:59:32 +0100

    Add pin schema definition and update schemaTypes to include pin

--- sanity-backend/schemaTypes/index.js
+++ sanity-backend/schemaTypes/index.js
@@ -1,3 +1,4 @@
-import user from "./user";
+import pin from './pin'
+import user from './user'
 
-export const schemaTypes = [user];
+export const schemaTypes = [user, pin]
---
+++ sanity-backend/schemaTypes/pin.js
@@ -0,0 +1,63 @@
+const pin = {
+  name: 'pin',
+  title: 'Pin',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'about',
+      title: 'About',
+      type: 'string',
+    },
+
+    {
+      name: 'destination',
+      title: 'Destination',
+      type: 'url',
+    },
+
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+    },
+
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'string',
+      Options: {
+        hotspot: true,
+      },
+    },
+
+    {
+      name: 'userId',
+      title: 'UserId',
+      type: 'string',
+    },
+    {
+      name: 'postedBy',
+      title: 'Posted By',
+      type: 'postedBy',
+    },
+    {
+      name: 'save',
+      title: 'Save',
+      type: 'array',
+      of: [{type: 'save'}],
+    },
+    {
+      name: 'comments',
+      title: 'Comments',
+      type: 'array',
+      of: [{type: 'comment'}],
+    },
+  ],
+}
+
+export default pin
