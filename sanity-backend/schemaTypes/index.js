68608c6efb9a1b307148bda9afb506981e1434ff
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-06 18:08:54 +0100

    Add save schema definition and update schemaTypes to include save

--- sanity-backend/schemaTypes/index.js
+++ sanity-backend/schemaTypes/index.js
@@ -1,5 +1,6 @@
 import comment from './comment'
 import pin from './pin'
+import save from './save'
 import user from './user'
 
-export const schemaTypes = [user, pin, comment]
+export const schemaTypes = [user, pin, comment, save]
