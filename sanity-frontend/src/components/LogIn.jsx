aa03be7649dcc764ea4d7842214fadaa545ab671
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-07 16:49:06 +0100

    Enhance LogIn component with video background and logo overlay

--- sanity-frontend/src/components/LogIn.jsx
+++ sanity-frontend/src/components/LogIn.jsx
@@ -1,5 +1,27 @@
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+
 const LogIn = () => {
-  return <div className="font-extrabold text-5xl">Welcome to LogIn page</div>;
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          autoPlay
+          loop
+          muted
+          className="object-cover w-full h-full"
+        />
+
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" alt="logo" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
