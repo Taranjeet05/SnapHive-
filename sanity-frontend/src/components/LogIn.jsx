7e37d5e06467587da2121d4c4440b7ab7c77ce0d
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-07 16:34:32 +0100

    Refactor LogIn component for cleaner syntax and add logowhite image asset

--- sanity-frontend/src/components/LogIn.jsx
+++ sanity-frontend/src/components/LogIn.jsx
@@ -1,9 +1,5 @@
 const LogIn = () => {
-  return (
-    <div className="font-extrabold text-5xl">
-      Welcome to LogIn page
-    </div>
-  )
-}
+  return <div className="font-extrabold text-5xl">Welcome to LogIn page</div>;
+};
 
-export default LogIn
+export default LogIn;
