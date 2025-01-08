dfda4e69304fb83ab168a4d9fa318ff7792ba975
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-08 18:28:54 +0100

    Add SideBar and UserProfile components with initial structure

--- sanity-frontend/src/container/Home.jsx
+++ sanity-frontend/src/container/Home.jsx
@@ -2,7 +2,11 @@ import { useState, useRef, useEffect } from "react";
 import {HiMenu} from "react-icons/hi";
 import {AiFiLLCloseCircle} from "react-icons/ai";
 import { Link, Route, Routes } from "react-router-dom";
+
 import { SideBar, UserProfile } from "../components";
+import Pins from "./Pins";
+import {client} from "../client";
+import logo from "../assets/logo.png";
 
 const Home = () => {
   return <div className="font-extrabold">Welcome to Home page</div>;
