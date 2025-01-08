148f91b1ffb3ff50bb90e437471047ad2ab33532
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-08 18:43:59 +0100

    Implement responsive layout and sidebar toggle functionality in Home component

--- sanity-frontend/src/container/Home.jsx
+++ sanity-frontend/src/container/Home.jsx
@@ -1,15 +1,35 @@
 import { useState, useRef, useEffect } from "react";
-import {HiMenu} from "react-icons/hi";
-import {AiFiLLCloseCircle} from "react-icons/ai";
+import { HiMenu } from "react-icons/hi";
+//import {AiFiLLCloseCircle} from "react-icons/ai";
 import { Link, Route, Routes } from "react-router-dom";
 
 import { SideBar, UserProfile } from "../components";
 import Pins from "./Pins";
-import {client} from "../client";
+//import {client} from "../client";
 import logo from "../assets/logo.png";
 
 const Home = () => {
-  return <div className="font-extrabold">Welcome to Home page</div>;
+  const [toggleSideBar, setToggleSideBar] = useState(false);
+
+  return (
+    <>
+      <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
+        <div className="hidden md:flex h-screen flex-initial">
+          <SideBar />
+        </div>
+        <div className="flex md:hidden flex-row">
+          <HiMenu
+            fontSize={40}
+            className="cursor-pointer"
+            onClick={() => setToggleSideBar()}
+          />
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28" />
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
