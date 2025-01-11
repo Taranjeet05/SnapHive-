dbe8a65e9b1078e6dc1b31498a8d6c1257655dae
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-11 09:13:07 +0100

    Refactor Pins component to include routing for Feed, PinDetail, CreatePin, and Search; add PropTypes validation for user prop

--- sanity-frontend/src/container/Pins.jsx
+++ sanity-frontend/src/container/Pins.jsx
@@ -1,12 +1,46 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import { Navbar, Feed, PinDetail, CreatePin, Search } from "../components";
+import PropTypes from "prop-types";
 
-
-
-const Pins = () => {
-  return(
-   <div className=""></div>
+const Pins = ({ user }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <div className="px-2 md:px-5">
+      <div className="bg-gray-50">
+        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route
+            path="/pin-detail/:pinId"
+            element={<PinDetail user={user && user} />}
+          />
+          <Route
+            path="/create-pin"
+            element={<CreatePin user={user && user} />}
+          />
+          <Route
+            path="/search"
+            element={
+              <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            }
+          />
+        </Routes>
+      </div>
+    </div>
   );
 };
+
+Pins.propTypes = {
+  user: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    userName: PropTypes.string, // optional
+  }),
+};
+
 
 export default Pins;
