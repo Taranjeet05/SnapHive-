3419d039c0dd037b8190f3cbffe0f85211c05a1c
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-12 17:22:38 +0100

    Add CreatePin component and categories data; implement initial layout and state management

--- sanity-frontend/src/components/CreatePin.jsx
+++ sanity-frontend/src/components/CreatePin.jsx
@@ -1,5 +1,14 @@
+import { useState } from 'react';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
+import { MdDelete } from 'react-icons/md';
 
-const CreatePin = () => {
+import { categories } from '../utils/data';
+import { client } from '../client';
+import Spinner from './Spinner';
+
+
+const CreatePin = ({user}) => {
   return (
     <div>CreatePin</div>
   )
