bb23bc51da81986d466ff3da5175f2209f90b45b
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-12 17:54:48 +0100

    Add initial structure and state management for PinDetails component

--- sanity-frontend/src/components/PinDetails.jsx
+++ sanity-frontend/src/components/PinDetails.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import { MdDownloadForOffline } from "react-icons/md";
+import { Link, useParams } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+
+import { client, urlFor } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import { pinDetailMorePinQuery, pinDetailQuery } from "../utils/data";
+import Spinner from "./Spinner";
 
 const PinDetails = () => {
+  const { pinId } = useParams();
+  const [pins, setPins] = useState();
+  const [pinDetail, setPinDetail] = useState();
+  const [comment, setComment] = useState("");
+  const [addingComment, setAddingComment] = useState(false);
   return (
-    <div>PinDetails</div>
-  )
-}
+    <div className="">
+      PinDetails
+    </div>
+  );
+};
 
-export default PinDetails
+export default PinDetails;
