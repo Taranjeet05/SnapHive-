b53a5ead01af47a8c5405270afd9bcfc7c5fb4fa
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-12 17:24:30 +0100

    Add state management for CreatePin component: initialize fields for title, about, destination, category, imageAsset, and loading status

--- sanity-frontend/src/components/CreatePin.jsx
+++ sanity-frontend/src/components/CreatePin.jsx
@@ -9,6 +9,17 @@ import Spinner from './Spinner';
 
 
 const CreatePin = ({user}) => {
+  const [title, setTitle] = useState('');
+  const [about, setAbout] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [destination, setDestination] = useState();
+  const [fields, setFields] = useState();
+  const [category, setCategory] = useState();
+  const [imageAsset, setImageAsset] = useState();
+  const [wrongImageType, setWrongImageType] = useState(false);
+
+  const navigate = useNavigate();
+  
   return (
     <div>CreatePin</div>
   )
